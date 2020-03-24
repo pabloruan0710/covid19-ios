36b438757ad9fba5c650285968373526f0987ef0
Pablo Ruan <pabloruan@outlook.com>
2020-03-24 16:53:23 -0300

    Initial Commit

---
+++ coronavirus covid19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coronavirus covid19
+//
+//  Created by Pablo Ruan on 24/03/20.
+//  Copyright © 2020 Pablo Ruan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ coronavirus covid19Tests/coronavirus_covid19Tests.swift
@@ -0,0 +1,34 @@
+//
+//  coronavirus_covid19Tests.swift
+//  coronavirus covid19Tests
+//
+//  Created by Pablo Ruan on 24/03/20.
+//  Copyright © 2020 Pablo Ruan. All rights reserved.
+//
+
+import XCTest
+@testable import coronavirus_covid19
+
+class coronavirus_covid19Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
